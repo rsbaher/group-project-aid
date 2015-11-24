da05bb54a001ab58a5cb0248d0cfbc48a1486203
Rayce Rossum <Rayce.Rossum@gmail.com>
2015-11-24 14:17:00 -0700

    Register Commit with DB create for USERS

---
+++ includes/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "127.0.0.1");    // The host you want to connect to.
+define("USER", "sec_user");     // The database username. 
+define("PASSWORD", "Password123!");         // The database password. 
+define("DATABASE", "group-project-aid");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hello, it's me.</h1>
+<?php
+$artist = "Adele";
+echo "Artist: ".$artist;
+?>
+</body>
+</html>
